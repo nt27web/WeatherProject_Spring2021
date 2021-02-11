f9b3a82807dac601bfb5b150c40418233f510c5e
nt27web <MS4Naina@2020>
2021-02-10 23:14:16 -0500

    completed graphs

--- main.py
+++ main.py
@@ -9,18 +9,24 @@ def recommendGolfer():
     json_object = getData()
     # print raw data
     json_formatted_str = json.dumps(json_object, indent=2)
-    print(json_formatted_str)
+    # print(json_formatted_str)
+
     # select the columns required for the analysis
     df = pd.DataFrame.from_records(json_object['data']
-        , columns=['valid_date', 'wind_gust_spd', 'wind_spd', 'uv', 'vis', 'precip']
+        , columns=['valid_date', 'wind_gust_spd', 'vis', 'precip', 'max_temp']
         )
+
     # # add a column to derive the recommendation based on the rule set in conditions method
     df['recommended'] = df.apply(conditions, axis=1)
-    df.plot(x="valid_date", y=['wind_gust_spd', 'vis', 'precip'])
-    plt.show()
 
-    plt.plot('valid_date', 'recommended', data=df, linestyle='none', marker='o')
-    plt.show()
+    print(df)
+
+    # df.plot(x="valid_date", y=['wind_gust_spd', 'vis', 'precip', 'max_temp'])
+    # plt.legend(['Wind Speed', 'Visibility', 'Precipitation', 'Max Temperature'])
+    # plt.show()
+    #
+    # plt.plot('valid_date', 'recommended', data=df, linestyle='none', marker='o')
+    # plt.show()
 
 
 def getData():
@@ -29,14 +35,14 @@ def getData():
         'x-rapidapi-key': "aa9ab8c454msh05a5f4ee87de0c3p18d923jsn59ae419e8e70",
         'x-rapidapi-host': "weatherbit-v1-mashape.p.rapidapi.com"
     }
-    conn.request("GET", "/forecast/daily?lat=38.5&lon=-78.5", headers=headers)
+    conn.request("GET", "/forecast/daily?lat=74.0&lon=40.7", headers=headers)
     return json.loads(conn.getresponse().read().decode("utf-8"))
 
 
 def conditions(df):
     if (df['wind_gust_spd'] < 7
             and df['precip'] < 1
-            and df['vis'] > 10):
+            and df['vis'] > 10 ):
         return 1 # recommended = yes
     else:
         return 0 # recommended = no
